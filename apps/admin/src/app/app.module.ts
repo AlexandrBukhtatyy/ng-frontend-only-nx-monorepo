ec673e380ae79b23f24a4772379c45b54d11240b
a.bukhtatiy <buhtatyalexander90@gmail.com>
2023-01-28 22:31:09 +0300

    add taiga ui styles to admin project

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {TuiButtonModule} from "@taiga-ui/core";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, TuiButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
